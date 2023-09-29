a7d7b61f805d47fbe4fef80a9c9b6857586d2ee8
Vicky Poonia <114798756+vicky-poonia@users.noreply.github.com>
2023-09-30 01:08:40 +0530

    ltkndyhrlgf

--- script.js
+++ script.js
@@ -11,25 +11,25 @@ window.addEventListener("DOMContentLoaded",getData)
 
 // Taking input from user
 async function addData(e) {
-    e.preventDefault();
+  e.preventDefault();
 
     let p_name = document.getElementById('name').value;
     let p_price = document.getElementById('price').value;
     let s_category = document.getElementById('select').value;
 
-    let details = {
-        p_name,
-        p_price,
+  let details = {
+    p_name,
+    p_price,
         s_category
-    };
+  };
 
-    try {
+  try {
         const response = await axios.post("https://crudcrud.com/api/09249a3661b14fbcbcc784fe8c2e866e/callBookings", details);
-        console.log(response.data);
-        showonScreen(response.data);
-    } catch (err) {
-        console.log(err);
-    }
+    console.log(response.data);
+    showonScreen(response.data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 
@@ -37,83 +37,108 @@ async function addData(e) {
 function showonScreen(user) {
     let flist = document.getElementById('flist');
     let clist = document.getElementById('clist');
-    let elist = document.getElementById("elist");
+  let elist = document.getElementById("elist");
 
-    let child = `<li id="${user._id}">${user.p_name} ${user.p_price} ${user.s_category}
+  let child = `<li id="${user._id}">${user.p_name} ${user.p_price} ${user.s_category}
             <button onclick="deleteItemFromScreen('${user._id}')">delete</button>
             <button onclick="editUserItem('${user.p_name}','${user.p_price}','${user.s_category}','${user._id}')">edit</button>`;
 
 
-    if (user.s_category === "Electronic") {
-        flist.innerHTML += child;
+  if (user.s_category === "Electronic") {
+    flist.innerHTML += child;
         console.log(child)
     }
 
     else if (user.s_category === "Skin-Product") {
 
-        clist.innerHTML += child;
-        console.log(child);
+    clist.innerHTML += child;
+    console.log(child);
     }
     else {
-        elist.innerHTML += child;
-        console.log(child);
-    }
+    elist.innerHTML += child;
+    console.log(child);
+  }
 }
+
+function showo(user) {
+    let upd = document.getElementById("upd");
+    console.log(user);
+ 
+    let child = ` <button onclick="editUserItemFromBackend('${user}')">update</button>`;
+       upd.innerHTML += child;
+    console.log(child);
+  }
 
 // Get data from endpoint
 async function getData() {
-    try {
+  try {
         const response = await axios.get("https://crudcrud.com/api/09249a3661b14fbcbcc784fe8c2e866e/callBookings");
-        response.data.forEach((el) => {
-            showonScreen(el);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    response.data.forEach((el) => {
+      showonScreen(el);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// delete data from screen 
+// delete data from screen
 async function deleteItemFromScreen(userId) {
-    let itemToRemove = document.getElementById(userId);
-    itemToRemove.parentNode.removeChild(itemToRemove);
-    try {
-        await deleteDataFromBackend(userId);
-    } catch (err) {
-        console.log(err);
-    }
+  let itemToRemove = document.getElementById(userId);
+  itemToRemove.parentNode.removeChild(itemToRemove);
+  try {
+    await deleteDataFromBackend(userId);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function editItemFromScreen(userId) {
-    let itemToRemove = document.getElementById(userId);
-    itemToRemove.parentNode.removeChild(itemToRemove);
-
-    
+  let itemToRemove = document.getElementById(userId);
+  itemToRemove.parentNode.removeChild(itemToRemove);
+  showo(userId)
 }
 
 // delete data from backend
 async function deleteDataFromBackend(userId) {
-    try {
+  try {
         const res = await axios.delete(`https://crudcrud.com/api/09249a3661b14fbcbcc784fe8c2e866e/callBookings/${userId}`);
-    } catch (err) {
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function editUserItem(name,price,category,userId){
 
-    document.getElementById("name").value = name;
-    document.getElementById("price").value = price;
-    document.getAnimations("select").value = category;
-    editItemFromScreen(userId);
+  document.getElementById("name").value = name;
+  document.getElementById("price").value = price;
+  document.getAnimations("select").value = category;
+  editItemFromScreen(userId);
 
 }
 
-async function editUserItemFromBackend(userId){
-    try{
-        const res = await axios.put(`https://crudcrud.com/api/09249a3661b14fbcbcc784fe8c2e866e/callBookings/${userId}`);
-    }catch(err){
-        console.log(err)
-    }
+async function editUserItemFromBackend(userId) {
+    console.log("120 - hitting me..");
+    let p_name = document.getElementById("name").value;
+    let p_price = document.getElementById("price").value;
+    let s_category = document.getElementById("select").value;
+  
+    let details = {
+      p_name,
+      p_price,
+      s_category,
+    };
+    console.log("130details-", details);
+    console.log(userId);
+
+  try {
+    const res = await axios.put(
+      `https://crudcrud.com/api/cfb3b2056bc04306bc8c398d4cc46769/callBookings/${userId}`, 
+      details
+    );
+    console.log("125 :-", res);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 
